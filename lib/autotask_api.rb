69afc17c9c64a6f6937b9170a9ab95e4fa15156f
Kevin Pheasey <kevin@kpheasey.com>
2016-08-18 13:12:45 -0400

    change load order

--- lib/autotask_api.rb
+++ lib/autotask_api.rb
@@ -1,12 +1,12 @@
 require 'nokogiri'
 require 'savon'
 
-require 'autotask_api/account'
-require 'autotask_api/client'
-require 'autotask_api/config'
-require 'autotask_api/entity'
-require 'autotask_api/query'
 require 'autotask_api/version'
+require 'autotask_api/config'
+require 'autotask_api/client'
+require 'autotask_api/query'
+require 'autotask_api/entity'
+require 'autotask_api/account'
 
 module AutotaskApi
 
