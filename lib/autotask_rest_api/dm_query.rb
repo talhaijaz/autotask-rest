42896b2309cbf4b67351335a122eaf0e77f8407f
talhaijaz <talhaijaz123451@gmail.com>
2022-09-09 03:30:04 +0500

    feat: autotask patch request functionality

--- lib/autotask_rest_api/dm_query.rb
+++ lib/autotask_rest_api/dm_query.rb
@@ -1,22 +1,35 @@
 module AutotaskRestApi
-    class DMQuery #Data manipulation query
-      attr_accessor :entity, :client, :body
-  
-      def initialize(entity, body, client = AutotaskRestApi.client)
-        @entity = entity
-        @client = client
-        @body = body
-      end
-  
-      def fetch
-        response = client.post_data entity, body
-     
-        if response.code == 200
-          "#{entity} Created Successfully!"
-        else
-          JSON(response.body)
-        end
-      end
+  class DMQuery # Data manipulation query
+    attr_accessor :entity, :client, :body, :request_type
+
+    def initialize(entity, body, client = AutotaskRestApi.client, request_type)
+      @entity = entity
+      @client = client
+      @body = body
+      @request_type = request_type
+    end
+
+    def fetch
+      response = if request_type == 'patch'
+                   client.patch_data entity, body
+                 else
+                   client.post_data entity, body
+                 end
+
+      res_body = if response.code == 200
+                  if request_type == 'patch'
+                     "#{entity} Updated Successfully!"
+                  elsif request_type == 'get'
+                      JSON(response.body)
+                  else
+                     "#{entity} Created Successfully!"
+                   end
+                 elsif response.code == 500 || response.code == 404
+                   response.body
+                 else
+                   JSON(response.body)
+                 end
+      OpenStruct.new({ code: response.code, body: res_body })
     end
   end
-  
+end
