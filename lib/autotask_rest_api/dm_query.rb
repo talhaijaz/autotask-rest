aa70dbd013e1a714abb5ec735f24e3a1100f4441
talhaijaz <talhaijaz123451@gmail.com>
2022-10-09 14:35:25 +0500

    fix: get request not working

--- lib/autotask_rest_api/dm_query.rb
+++ lib/autotask_rest_api/dm_query.rb
@@ -2,36 +2,32 @@ module AutotaskRestApi
   
   # Data manipulation query
   class DMQuery 
-    attr_accessor :entity, :client, :condition, :request_type
+    attr_accessor :entity, :client, :request_body, :request_type
 
-    def initialize(entity, condition, client = AutotaskRestApi.client, request_type)
+    def initialize(entity, body, client = AutotaskRestApi.client, request_type)
       @entity = entity
       @client = client
-      @condition = condition
+      @request_body = body
       @request_type = request_type
     end
 
     def call_autotask
-      response = if request_type == 'get'
-                   client.get_data entity, condition
-                 elsif request_type == 'patch'
-                   client.patch_data entity, condition
+      response = if request_type == 'patch'
+                   client.patch_data entity, request_body
                  else
-                   client.post_data entity, condition
+                   client.post_data entity, request_body
                  end
 
       res_body = if response.code == 200
-                    if request_type == 'get'
-                      JSON(response.body)
-                    elsif request_type == 'patch'
+                    if request_type == 'patch'
                       "#{entity} Updated Successfully!"
                     else
                       "#{entity} Created Successfully!"
                     end
                  elsif response.code == 500 || response.code == 404 || response.code == 401
-                   response.body
+                    response.body
                  else
-                   JSON(response.body)
+                    JSON(response.body)
                  end
       OpenStruct.new({ code: response.code, body: res_body })
     end
